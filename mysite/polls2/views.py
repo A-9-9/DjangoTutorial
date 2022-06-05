576de4190fec3206534d805a6fa097381df462df
Ken using VM <10846013@ntub.edu.tw>
2022-06-06 00:03:47 +0800

    Complete DB config and 2 model(Question, Choice)

--- mysite/polls2/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-def index(request):
-	return HttpResponse("Hello, world. U r in the polls2.view.index function.")
