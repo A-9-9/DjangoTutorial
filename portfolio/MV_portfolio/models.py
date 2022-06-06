c97cd42c3e7b689ad2962123710978e1ccd3061f
Gituser111111 <10846019@ntub.edu.tw>
2022-06-06 19:34:09 +0800

    model2

--- portfolio/MV_portfolio/models.py
+++ portfolio/MV_portfolio/models.py
@@ -7,9 +7,12 @@ class User(models.Model):
 	user_account = models.CharField(max_length=50)
 	user_password = models.CharField(max_length=50)
 
+	def __str__(self):
+		return self.user_name
 
 class Department(models.Model):
 	name = models.CharField(max_length=100)
+
 
 
 class Company(models.Model):
@@ -17,23 +20,17 @@ class Company(models.Model):
 	department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
 
-class Invest_combination(models.Model):
-	company = models.ForeignKey(Company, on_delete=models.CASCADE)
-	weights = models.ArrayField(CharField(max_length=50))
-	shares = models.ArrayFailed(CharField(max_length=50))
-	#conbination_id
-
-
 class Member_invest(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	combination = models.ForeignKey(Invest_combination, on_delete=models.CASCADE)
+	combination = models.ForeignKey(Information, on_delete=models.CASCADE)
 	date = models.DateTimeField()
 	total = models.IntegerField(default=0)
 
 
 
-
-class Price(models.Model):
+class Information(models.Model):
 	company = models.ForeignKey(Company, on_delete=models.CASCADE)
 	date = models.DateTimeField('Closing price')
 	ROI = models.FloatField(default=0)
+	weights = models.ArrayField(CharField(max_length=50))
+	shares = models.ArrayFailed(CharField(max_length=50))
